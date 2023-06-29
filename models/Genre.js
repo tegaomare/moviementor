8b03d58bc8d2adf275a111d47bbb8c4510a584f3
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-29 11:46:33 -0400

    added id

--- models/Genre.js
+++ models/Genre.js
@@ -1,2 +1,18 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Genre extends Model {}
+
+Genre.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        genre: {
+            
+        }
+    }
+)
