eff71dd3a34204d15c5b9b8b5b070eaec3b3ba40
tegaomare <tegaomare@yahoo.com>
2023-06-29 12:00:41 -0400

    all models for movies added

---
+++ models/Movie.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Movie extends Model {}
+
+Movie.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: "genre",
+        key: "id",
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    rating_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: "rating",
+        key: "id",
+      },
+    },
+
+    sex_rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    violence_rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    language: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    timestamps: false,
+    modelName: "movie",
+  }
+);
+
+module.exports = Movie;
