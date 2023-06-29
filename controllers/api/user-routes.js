066912572fbfc3633b77c536d31f94114bd80115
Dan G <daniel.w.gray1994@gmail.com>
2023-06-29 17:27:17 -0400

    added user for get by id and user route for create a new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,6 +3,17 @@ const User = require("../../models/User.js");
 
 // GET one user
 // GET /api/users/:id
+router.get("./:id", async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params);
+    if (!userData) {
+      res.status(404).json({ message: "No user with this id!" });
+    }
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // POST create a new user
 // POST /api/users
