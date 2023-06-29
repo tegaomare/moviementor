5e7676bd718349efe63d87fb0e4bd386eac5a5fb
Dan G <daniel.w.gray1994@gmail.com>
2023-06-29 14:18:12 -0400

    added pseudo code to the user-routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const User = require("../../models/User.js");
+
+// GET one user
+// GET /api/users/:id
+
+// POST create a new user
+// POST /api/users
+
+// POST user login route
+// POST /api/users/login
+
+// PUT update a user
+// PUT /api/users/:id
