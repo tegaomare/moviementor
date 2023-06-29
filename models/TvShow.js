ef924f8a9355a74b6542b6fb4cc1ee4299d3c4c5
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-29 10:47:02 -0400

    started tvshow model

---
+++ models/TvShow.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class TvShow extends Model {}
+
+TvShow.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        genre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        num_of_seasons: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        rating: {
+            type: DataTypes.STRING,
+            
+        }
+    }
+)
