6c36bb067db1bc88ec9ba0a1517f154e9537a323
tegaomare <tegaomare@yahoo.com>
2023-06-29 10:53:32 -0400

    id datatype added

--- models/movie.js
+++ models/movie.js
@@ -2,3 +2,22 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class Movie extends Model {}
+
+Movie.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "gallery",
+  }
+);
+
+module.exports = Movie;
