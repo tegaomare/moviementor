77a7acaf9bc9dbb0893d389ce3daaa701a0577e9
Dan G <daniel.w.gray1994@gmail.com>
2023-06-29 14:39:32 -0400

    added a POST route, not yet fully functioning

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,9 +6,19 @@ const User = require("../../models/User.js");
 
 // POST create a new user
 // POST /api/users
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // POST user login route
 // POST /api/users/login
 
 // PUT update a user
 // PUT /api/users/:id
+
+module.exports = router;
