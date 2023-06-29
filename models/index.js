e496ba29072e3e136cd73e1e3f057a2f4d2541f9
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-29 18:32:50 -0400

    updated connection js

--- models/index.js
+++ models/index.js
@@ -7,7 +7,20 @@ const Rating = require('./Rating');
 
 // NEED ASSOCIATIONS
 
+//genre has many movies
+//genre has many tvshows
 
+//rating has many movies
+//rating has many tvshows
+
+//category has many movies
+//category has many tvshows
+
+//movie belongs to category
+//tvshow belongs to category
+
+//user has many movies
+//user has many tv shows
 
 
 
