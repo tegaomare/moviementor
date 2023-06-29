3fb79f3de99c87a8eeee508d71be8862f8a11af2
Lauren Miller <lmiller90@fordham.edu>
2023-06-29 11:14:39 -0400

    compeleted category model

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,28 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Movie extends Model {}
+class Category extends Model {}
 
-Movie.init(
+Category.init(
     {
         id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+    },
+    {
+        sequalize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category'
     }
-)
+);
+
+module.exports = Category;
