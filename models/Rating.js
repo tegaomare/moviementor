227397a2456a0611594e6ec45da967426227f58d
Lauren Miller <lmiller90@fordham.edu>
2023-06-29 12:11:55 -0400

    completed init

--- models/Rating.js
+++ models/Rating.js
@@ -3,3 +3,30 @@ const sequelize = require('../config/connection');
 
 class Rating extends Model {}
 
+Rating.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        rating: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequalize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'rating'
+    }
+);
+
+module.exports = Rating;
