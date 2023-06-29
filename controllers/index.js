c5381dbd87693fb66a53723d84c8eb9ebe59ac2f
Dan G <daniel.w.gray1994@gmail.com>
2023-06-29 14:21:04 -0400

    added an api route to the index.js inside controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const homeRoutes = require("./home-routes");
+const apiroutes = require("./api");
 
 router.use("/", homeRoutes);
+router.use("/api", apiroutes);
 
 module.exports = router;
