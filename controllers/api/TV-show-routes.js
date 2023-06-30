d1dfafbe226a4f3a24b97ad3c63a655c4b064e30
Dan G <daniel.w.gray1994@gmail.com>
2023-06-30 12:04:40 -0400

    added a GET route for all TV shows

--- controllers/api/TV-show-routes.js
+++ controllers/api/TV-show-routes.js
@@ -1,4 +1,18 @@
 const router = require("express").Router();
 const TvShow = require("../../models/TvShow");
 
+// GET all tv shows
+// GET /api/tv-shows
+router.get("/", async (req, res) => {
+  try {
+    const tvShowData = TvShow.findAll();
+    if (!tvShowData) {
+      return res.status(404).json({ message: "No movies are available!" });
+    }
+    res.status(200).json(tvShowData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
