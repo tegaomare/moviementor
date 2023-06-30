ff4c7d3c8cbbb9fbe817c97b4de85375391df1ee
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-30 14:33:22 -0400

    added userdata seeds

--- seeds/seeddummy.js
+++ seeds/seeddummy.js
@@ -5,3 +5,14 @@ const userData = require('./userData.json');
 const movieData = require('./movieData.json');
 const tvshowData = require('./tvshowData.json');
 
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+};
+
+dummyseedDatabase();
