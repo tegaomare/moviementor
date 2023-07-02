e42b9540b03d97ccdd90a27980e89515f55b3ac9
Lauren Miller <lmiller90@fordham.edu>
2023-07-02 13:26:00 -0400

    included req session save for new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -36,7 +36,14 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
-    res.status(200).json(userData);
+    
+    req.session.save(() => {
+      req.session.loggedIn = true;
+    
+    res.status(200).json(userData)
+    });
+  
+  
   } catch (error) {
     res.status(400).json(error);
   }
