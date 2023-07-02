956e6f455533df6695620fcddc577ed3c55ad454
Lauren Miller <lmiller90@fordham.edu>
2023-07-02 13:35:02 -0400

    fixed login value

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 //need auth for if user is not logged in, and then redirect them to homepage
 const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
+    if (!req.session.loggedIn) {
         res.redirect("/login")
     } else {
         next();
